d8fea6b4ef6841f6c4d89477880c4aef52b930db
Emilio J. Montes <emiliomontesv@hotmail.com>
2021-08-23 09:42:05 -0500

    add . file

---
+++ home.php
@@ -0,0 +1,296 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- ===== BOX ICONS ===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="style/grid.css">
+        <link rel="stylesheet" href="style/scroll_bar.css">
+        <link rel="stylesheet" href="style/flip_img.css">
+        <link rel="stylesheet" href="style/noselect.css">
+        <link rel="stylesheet" href="style/app_3.css">
+        
+
+        <title>Home</title>
+        <script src="includes/templates/js/main.js"></script>
+    </head>
+    <body id="body-pd" class="noselect">
+
+        <!--Nav-->
+        <?php include_once 'includes/templates/header.php'?>
+        <!--Nav-->
+
+        <div class="grid">
+
+
+            <!-- Grid ----------------------------------------->
+
+            
+            <div class="element">
+                <div class="titulo">
+                    Leyes
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/leyes.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Abogados
+                        <br>
+                        -Notarios
+                        <br>
+                        -Despachos
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Comunicacion
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/Reportero.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Reporteros
+                        <br>
+                        -Conductores
+                        <br>
+                        -Publicistas
+                        <br>
+                        -Fotografos
+                        <br>
+                        -Camarografos
+                        <br>
+                        -Locutores
+                        <br>
+                        -Operedores
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Medica
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/medico.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Medicos
+                        <br>
+                        -Enfermeros
+                        <br>
+                        -Fisioterapeutas
+                        <br>
+                        -Especialistas
+                        <br>
+                        -Psicologos
+                        <br>
+                        -Terapeutas de Lenguaje
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Industrial
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/industrias.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Ingenieros
+                        <br>
+                        -Obreros
+                        <br>
+                        -Operadores
+                        <br>
+                        -Tecnicos
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Oficios
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/albañil.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Plomeros
+                        <br>
+                        -Pintores
+                        <br>
+                        -Carpinteros
+                        <br>
+                        -Amas de casa
+                        <br>
+                        -Electricistas
+                        <br>
+                        
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Publicidad
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/Publicidad.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Diseño Grafico
+                        <br>
+                        -Periodicos
+                        <br>
+                        -Television
+                        <br>
+                        -Mercadotecnias
+                        <br>
+                        -Publicistas
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Construccion
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/construccion.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Operadores
+                        <br>
+                        Albañiles
+                        <br>
+                        Constructoras
+                        <br>
+                        -Arquitectos
+                        <br>
+                        -Ingenieros
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Educativo
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/educacion.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Maestros
+                        <br>
+                        -Educacion Especial
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Gatronomia
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/gastronomia.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        En esta seccion puedes encontrar:
+                        <hr>
+                        -Cocineros
+                        <br>
+                        -Ayudantes de cocina
+                        <br>
+                        -Lavaplatos
+                        <br>
+                        -Banquetes
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="element">
+                <div class="titulo">
+                    Programacion
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/programacion.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        Hola xD
+                    </div>
+                </div>
+            </div>
+
+            <div class="element">
+                <div class="titulo">
+                    Finanzas
+                </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="img/finanzas.jpg" alt="" class="img_back">
+                    </div>
+                    <div class="flip-card-back">
+                        Hola xD
+                    </div>
+                </div>
+            </div>
+
+            <div class="element">
+                <div class="titulo">
+                    Ambiental
+                </div>
+                <img src="img/programacion.jpg" alt="">
+            </div>
+        </div>
+
+
+        <!--===== MAIN JS =====-->
+        <script src="includes/templates/js/nav.js"></script>
+    </body>
+</html>
